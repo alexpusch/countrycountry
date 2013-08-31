0b83bf511f29db653f51ec772edbf56037739332
Alex <alex@local.host>
2013-08-31 15:00:35 +0300

    Fix countries path

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ require 'country_provider.rb'
 
 class MainController < ApplicationController
   def index
-    country_provider = CountiesProvider.new "app/assets/countries"
+    country_provider = CountiesProvider.new "public/countries"
     gon.countries = country_provider.get_countries_list
   end
 end
